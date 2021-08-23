c91fb9a9fe7fd682ccf47fe91a9d4e0d76557a35
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2021-08-23 01:21:57 -0400

    Added missing file from last commit

---
+++ ArkhamDisplay/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ArkhamDisplay{
+	class Utils{
+		public static string GetSHA1Hash(string filePath){
+			if(!System.IO.File.Exists(filePath)){
+				return "";
+			}
+
+			return GetSHA1Hash(System.IO.File.ReadAllBytes(filePath));
+		}
+
+		public static string GetSHA1Hash(byte[] data){
+			using var cryptoProvider = new SHA1CryptoServiceProvider();
+			return BitConverter.ToString(cryptoProvider.ComputeHash(data));
+		}
+	}
+}
