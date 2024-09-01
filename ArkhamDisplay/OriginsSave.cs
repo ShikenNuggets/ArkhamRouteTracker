c1e50dfdfa63ee6d7244be146c36d73413c694dd
ShikenNuggets <carter.rennick@gmail.com>
2024-08-31 22:48:37 -0400

    Fixed bug with DarkKnightFinish check on alternate IDs

--- ArkhamDisplay/OriginsSave.cs
+++ ArkhamDisplay/OriginsSave.cs
@@ -8,9 +8,11 @@ namespace ArkhamDisplay{
 			if("Data Handler".Equals(entry.type)){
 				return HasKey("SS_Enigma_EnigmaHQ_Visited", requiredMatches) && !HasKey(entry.id, requiredMatches) && !HasKey(entry.alternateID, requiredMatches);
 			}
-			else if(!string.IsNullOrWhiteSpace(entry.metadata) && entry.metadata.Contains("DarkKnightFinish")){
-				string id = entry.metadata.Contains("ALTERNATE") ? entry.alternateID : entry.id;
-				return HasKeyCustomRegex(@"\b" + id + @"............" + Convert.ToChar(Convert.ToByte("0x1", 16)), requiredMatches);
+			else if("DarkKnightFinish".Equals(entry.metadata)){
+				return HasKeyCustomRegex(@"\b" + entry.id + @"............" + Convert.ToChar(Convert.ToByte("0x1", 16)), requiredMatches);
+			}else if(!string.IsNullOrWhiteSpace(entry.metadata) && entry.metadata.Contains("DarkKnightFinish") && entry.metadata.Contains("ALTERNATE")){
+				bool hasAlternate = HasKeyCustomRegex(@"\b" + entry.alternateID + @"............" + Convert.ToChar(Convert.ToByte("0x1", 16)), requiredMatches);
+				return hasAlternate || base.HasKey(entry.id, requiredMatches);
 			}
 
 			return base.HasKey(entry, requiredMatches);
